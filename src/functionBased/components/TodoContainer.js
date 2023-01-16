f40995bd70df9708a709829df64ee7f2e3085125
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-16 16:24:35 +0200

    Fix a bug

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import TodosLists from './TodoLists';
-import About from '../pages/About';
-import NotMatch from '../pages/NotMatch';
-import Navbar from '../pages/Navbar';
 
 export default function TodoContainer() {
   function getInitialTodos() {
@@ -60,29 +56,17 @@ export default function TodoContainer() {
   };
 
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <div className="container">
-              <div className="inner">
-                <Header />
-                <InputTodo addTodoItem={addTodoItem} />
-                <TodosLists
-                  todos={todos}
-                  handleChange={handleChange}
-                  delTodo={delTodo}
-                  setUpdate={setUpdate}
-                />
-              </div>
-            </div>
-          )}
+    <div className="container">
+      <div className="inner">
+        <Header />
+        <InputTodo addTodoItem={addTodoItem} />
+        <TodosLists
+          todos={todos}
+          handleChange={handleChange}
+          delTodo={delTodo}
+          setUpdate={setUpdate}
         />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
-    </>
+      </div>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
-
 // stylesheet
 import './functionBased/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <TodoContainer />
-    </Router>
+    <TodoContainer />
   </React.StrictMode>,
 );
 
