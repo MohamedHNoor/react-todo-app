c7d7cb89eca8937d580a5c5b6767aa617d6e242c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-18 15:39:24 +0200

    Fix error

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -7,13 +7,13 @@ const SinglePage = () => {
       slug: 'about-app',
       title: 'About the App',
       description:
-        'In this app, you can add, delete, submit, and edit items. To edit items, double-click on them. Once you are done, press the enter key to resubmit. This app will persist your data in the browser's local storage. So whether you reload, close your app, or reopen it, you still have access to your to-dos items.',
+        "In this app, you can add, delete, submit, and edit items. To edit items, double-click on them. Once you are done, press the enter key to resubmit. This app will persist your data in the browser's local storage. So whether you reload, close your app, or reopen it, you still have access to your to-dos items.",
     },
     {
       slug: 'about-author',
       title: 'About the Author',
       description:
-        'Mohamed Hassan Noor, a Full-stack web developer, developed this app. I am open to collaborating on projects or freelance Gigs. Connect with me on LinkedIn: https://www.linkedin.com/in/mohamedhnoor/.',
+        'Mohamed Hassan Noor, a Full-stack web developer built this app. He is open to collaborate on projects. Connect with him on LinkedIn: https://www.linkedin.com/in/mohamedhnoor/.',
     },
   ];
   const { slug } = useParams();
